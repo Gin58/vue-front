b95e1ad05fe52f69de590967dc9a94961c3d6429
Gin58 <invincible.gnj1875@gmail.com>
2020-07-13 21:07:37 +0900

    add changeProductStatus

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
     <h1>お買い物リスト</h1>
+    <p>
+      品名：
+      <input 
+        type="text" 
+        name='productName' 
+        v-model="productName" 
+        v-bind:class="{'alert-color': !validate }" 
+        value='' 
+        size="40" 
+        placeholder="品名を入力してください※必須"
+      >
+    </p>
+    <p>
+      メモ：
+      <input 
+        type="text" 
+        name='productMemo' 
+        v-model="productMemo" 
+        value='' 
+        size="40"
+      >
+    </p>
+
+    <button 
+      v-on:click="doAddProduct" 
+      v-bind:disabled="!isEntered"
+    >
+      追加
+    </button>
     <table>
         <thead v-pre>
             <tr>
@@ -8,6 +37,7 @@
                 <th class="name">商品名</th>
                 <th class="memo">メモ</th>
                 <th class="state">状態</th>
+                <th class="delete">削除</th>
             </tr>
         </thead>
         <tbody>
@@ -18,7 +48,16 @@
               <td class="index">{{ key + 1 }}</td>
               <td class="name">{{ item.name }}</td>
               <td class="memo">{{ item.memo }}</td>
-              <td class="state">{{ item.state }}</td>
+              <td class="state">
+                <button v-on:click="doChangeProductState(item)">
+                  {{ labels[item.state] }}
+                </button>
+              </td>
+              <td class="delete">
+                <button v-on:click="doDeleteProduct(item)">
+                    削除
+                </button>
+              </td>
             </tr>
         </tbody>
     </table>
@@ -58,6 +97,19 @@ export default Vue.extend({
     };
   },
   computed: {
+    labels(): {} {
+      return this.options.reduce((a, b) => {
+        return Object.assign(a, { [b.value]: b.label });
+      }, {});
+    },
+    validate(): boolean {
+      const isEnteredProductName = 0 < this.productName.length;
+      this.isEntered = isEnteredProductName;
+      return isEnteredProductName;
+    },
+  },
+  created() {
+    this.doFetchAllProducts();
   },
   mounted() {
     this.doFetchAllProducts();
@@ -67,11 +119,74 @@ export default Vue.extend({
       axios.get('http://localhost:8080/fetchAllProducts')
       .then((res) => {
           if (res.status !== 200) {
-              throw new Error('レスポンスエラー');
+            throw new Error('レスポンスエラー');
           } else {
-              this.products = res.data;
+            this.products = res.data;
           }
       });
+    },
+    doDeleteProduct(product: Product) {
+      const params = new URLSearchParams();
+      params.append('productID', `${product.id}`);
+
+      axios.post('http://localhost:8080/deleteProduct', params)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error('レスポンスエラー');
+        } else {
+          this.doFetchAllProducts();
+        }
+      });
+    },
+    doAddProduct() {
+      const params = new URLSearchParams();
+      params.append('productName', this.productName);
+      params.append('productMemo', this.productMemo);
+
+      axios.post('http://localhost:8080/addProduct', params)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error('レスポンスエラー');
+        } else {
+          this.doFetchAllProducts();
+          this.initInputValue();
+        }
+      });
+    },
+    doFetchProduct(product: Product) {
+      axios.get('http://localhost:8080/fetchProduct', {
+        params: {
+          productID: product.id,
+        },
+      })
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error('レスポンスエラー');
+        } else {
+          const resultProduct = res.data;
+          const index = this.products.indexOf(product);
+          this.products.splice(index, 1, resultProduct[0]);
+        }
+      });
+    },
+    doChangeProductState(product: Product) {
+      const params = new URLSearchParams();
+      params.append('productID', `${product.id}`);
+      params.append('productState', `${product.state}`);
+
+      axios.post('http://localhost:8080/changeStatusProduct', params)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error('レスポンスエラー');
+        } else {
+          this.doFetchProduct(product);
+        }
+      });
+    },
+    initInputValue() {
+      this.current = -1;
+      this.productName = '';
+      this.productMemo = '';
     },
   },
 });
@@ -79,6 +194,9 @@ export default Vue.extend({
 <style scoped lang="scss">
 .alert-color {
     border-color: #ff0000;
+}
+button {
+  margin-bottom: 10px;
 }
 table {
     border-collapse: collapse;
